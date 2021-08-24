3afe98e933f58d7d20df0cd147a918627cb700f3
samueltulus09 <samuel.pangaribuan@inphosoft.com>
2021-08-24 18:05:38 +0700

    Add project files.

---
+++ AuthApi/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["Jwt:IssuerSigningKey"]));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                    issuer: _config["Jwt:ValidIssuer"],
+                    audience: _config["Jwt:ValidAudience"],
+                    expires: DateTime.Now.AddHours(1),
+                    signingCredentials: signingCredentials
+                );
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
